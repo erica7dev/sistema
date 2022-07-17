9e0879b4e89748d5300a9c2d1a93539cf139a04c
erica7dev <erikaisa7dev@gmail.com>
2022-07-17 15:01:38 -0300

    add db

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-function aplicarDesconto(valor: number, desconto: number) {
-  if (desconto > valor) return 0;
-  return valor - desconto;
-}
+import express from "express";
+import cors from "cors";
+const app = express();
 
-module.exports = { aplicarDesconto };
-console.log(aplicarDesconto(10, 5));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+const UserRoutes = require("./routes/user.routes");
+const ProductRoutes = require("./routes/product.routes");
+
+app.use('/user', UserRoutes);
+app.use('/products', ProductRoutes);
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
+
